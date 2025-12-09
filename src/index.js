3bc7a282ff3356c987bcf64544ab6571e67077ed
Richard <53197263+ruizhelou@users.noreply.github.com>
2025-12-09 23:20:54 +1300

    Render home and menu via index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import { loadHomePage } from './homepage-loader.js'
 import { loadMenu } from './menu-loader.js'
 import "./styles.css"
 
-// loadHomePage()
-loadMenu()
+class DomRenderer {
+    homeButton = document.querySelector(".home-btn")
+    menuButton = document.querySelector(".menu-btn")
+    contactButton = document.querySelector(".contact-btn")
+
+    constructor() {
+        this.homeButton.addEventListener('click', event => this.render(loadHomePage))
+        this.menuButton.addEventListener('click', event => this.render(loadMenu))
+    }
+
+    render(loader) {
+        const content = document.querySelector("#content")
+        content.innerHTML = '';
+        loader()
+    }
+}
+
+const domRenderer = new DomRenderer()
+domRenderer.render(loadHomePage)
